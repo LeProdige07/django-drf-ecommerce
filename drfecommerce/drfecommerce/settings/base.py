3f062cd6067f99925e0df2560028e7c1634c5432
Don Makila <96491656+LeProdige07@users.noreply.github.com>
2025-07-17 11:06:54 +0100

    feat:fonctionnalities

--- drfecommerce/drfecommerce/settings/base.py
+++ drfecommerce/drfecommerce/settings/base.py
@@ -33,7 +33,9 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     # External Packages
     'rest_framework',
+    'drf_spectacular',
     # Internal Apps
+    "drfecommerce.product",
 
 ]
 
@@ -112,4 +114,11 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-REST_FRAMEWORK = {}
+REST_FRAMEWORK = {
+    'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema'
+}
+
+SPECTACULAR_SETTINGS = {
+    'TITLE': 'Django DRF Ecommerce',
+
+}
